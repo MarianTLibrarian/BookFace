ef73e7af25a30e4195421a4b6999f638d7bdcddf
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-05-09 14:54:02 -0500

    skeleton build

--- client/pages/MyBooks/index.jsx
+++ client/pages/MyBooks/index.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import axios from 'axios';
+import SideBar from './sidebar.jsx';
+import useStore from './useStore';
 
-export default function MyBooks() {
-  return <div>MyBooks</div>;
+const { personalBooks } = require('../../../server/controllers/books.js')
+
+const { user } = useStore();
+console.log(user);
+
+
+
+
+class BookShelf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageview: "",
+      books: []
+    }
+
+  }
+
+ componentDidMount() {
+   this.getBooks();
+ }
+
+ getBooks () {
+
+ }
+
+
+
+  render() {
+    return(
+      <div>
+        <div classname="sidebar">
+          <SideBar />
+
+        </div>
+      </div>
+    )
+  }
 }
+
+
---
+++ client/pages/MyBooks/sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const SideBar = () => {
+
+  return(
+    <div className="sidebar">
+      <h2>My Books</h2>
+      <h3>All</h3>
+      <h3>Read</h3>
+      <h3>Currently Reading</h3>
+      <h3>Want To Read</h3>
+    </div>
+  )
+}
+
+
+export default SideBar;
