d1815881a0e6b5be3ea78a34da0edc41ca956c76
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-09 19:36:01 -0400

    Feature: home page search composite layout (#9)
    * Show our `B` icon as the favicon
    
    * Add an initial layout for composite search bar
    
    * Replace textfield with SearchBar component
    
    I also made some small changes to the formatting and fixed some typos to
    get rid of ESLint errors.
    
    * Update styling
    
     - Dropdown no longer uses ButtonIcon. Just a Button works better
     - Typography size of filter text reduced Medium -> Small
     - SearchIcon button moved to Input endAdornment

--- client/pages/Home/index.jsx
+++ client/pages/Home/index.jsx
@@ -1,76 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../styles/Home.css';
 import Trends from './Trends';
 import BookClubs from './BookClubs';
+import SearchBar from '../../../components/SearchBar';
 
 export default function Home() {
-
-  const [trends, setTrends] = useState([1, 2, 3, 4])
-  const [bookClubs, setBookClubs] = useState([1, 2, 3, 4, 5, 6, 7, 8])
+  const [trends, setTrends] = useState([1, 2, 3, 4]);
+  const [bookClubs, setBookClubs] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
 
   const style = {
-    'background': 'url(../assets/header-bg.jpg) no-repeat center center fixed'
-  }
+    background: 'url(../assets/header-bg.jpg) no-repeat center center fixed',
+  };
 
   return (
-    <div className='Home'>
-      <div className='header-container'>
-        <div className='header' style={style}>
-          <div className='filter'></div>
-          <div className='main-content'>
-            <div>
-              <h1>BOOKFACE</h1>
-              <h3>A Home For Your Tome</h3>
-              <div className='search'>
-                <input placeHolder='Enter A Book Name' />
-              </div>
-              <button type='button'>SEARCH</button></div>
+    <div className="Home">
+      <div className="header-container">
+        <div className="header" style={style}>
+          <div className="filter" />
+          <div className="main-content">
+            <SearchBar />
           </div>
         </div>
       </div>
-      <div className='description'>
-        <div className='text'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor diam vitae interdum molestie. In placerat suscipit velit, gravida pretium velit maximus id. Duis ut felis maximus, suscipit orci vitae, porta metus. </p>
+      <div className="description">
+        <div className="text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor diam vitae
+            interdum molestie. In placerat suscipit velit, gravida pretium velit maximus id. Duis ut
+            felis maximus, suscipit orci vitae, porta metus.{' '}
+          </p>
         </div>
       </div>
 
-      <div className='trends'>
+      <div className="trends">
         <h1>TRENDS</h1>
         <p>Simple is Better.</p>
-        <div className='trends-list'>
-          {trends.map((book, index) => {
-            return <Trends book={book} key={index} />
-          })}
+        <div className="trends-list">
+          {trends.map((book) => (
+            <Trends book={book} key={book} />
+          ))}
         </div>
       </div>
 
-      <div className='featured-clubs'>
-        <div className='left'>
+      <div className="featured-clubs">
+        <div className="left">
           <div>
             <h3>FEATURED</h3>
             <h1>BOOK CLUBS</h1>
           </div>
         </div>
-        <div className='right'>
-          <div className='clubs-list'>
-            {bookClubs.map((club, index) => {
-              return <BookClubs club={club} key={index} />
-            })}
-            <div className='clear'></div>
+        <div className="right">
+          <div className="clubs-list">
+            {bookClubs.map((club) => <BookClubs club={club} key={club} />)}
+            <div className="clear" />
           </div>
         </div>
       </div>
 
-      <div className='surprise'>
+      <div className="surprise">
         <h1>SURPRISE ME!</h1>
-        <div class="card">
-          <div class="imgBox">
-            <div class="bark"></div>
-            <img src="../assets/logo.png" />
+        <div className="card">
+          <div className="imgBox">
+            <div className="bark" />
+            <img src="../assets/logo.png" alt="BookFace Logo" />
           </div>
-          <div class="details">
-            <h4 class="color1">READ KATY'S NOTION OR GO BACK TO SCHOOL!!!</h4>
-
+          <div className="details">
+            <h4 className="color1">READ KATY&apos;S NOTION OR GO BACK TO SCHOOL!!!</h4>
           </div>
         </div>
       </div>
