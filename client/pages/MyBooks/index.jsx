940464f30bd84fe915d551ad2164265c25d19b12
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-05-09 18:29:43 -0500

    implemented state hooks

--- client/pages/MyBooks/index.jsx
+++ client/pages/MyBooks/index.jsx
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SideBar from './sidebar';
+import Carousel from './carousel';
 import useStore from '../../userStore';
+import data from '../../../fakeData/books/personalBooks'
 
 const { router } = '../../../server/routes';
 
 
+
 export default function MyBooks() {
+
+
+  const [books, setBooks] = useState(data);
+
 
   const { user } = useStore();
    console.log('user', user)
 
-  const getBooks = function() {
-    axios.get('/books')
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
+  // const getBooks = function() {
+  //   axios.get('/books')
+  //   .then(data => {
+  //     console.log(data)
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   })
+  // }
 
-  const componentDidMount = function() {
-    getBooks()
-    console.log('mounted')
-  }
+useEffect (() => {
+    setBooks(data);
+  })
 
-  componentDidMount();
+
 
 
     return(
       <div>
         <div className="sidebar">
           <SideBar />
+          <Carousel books={books.results}/>
         </div>
       </div>
     )
