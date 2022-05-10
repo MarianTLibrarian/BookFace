5cd51520302a3b0ff0c128ac8749c83620364291
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-05-10 18:23:56 -0500

    exampleconfig fixed

--- components/Firebase/exampleConfig.js
+++ components/Firebase/exampleConfig.js
@@ -12,14 +12,4 @@ const firebaseConfig = {
 export default firebaseConfig;
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDmrL6uu488tGM-4ttKWIjcJ1mTHhqJhGU",
-  authDomain: "blueocean-library.firebaseapp.com",
-  projectId: "blueocean-library",
-  storageBucket: "blueocean-library.appspot.com",
-  messagingSenderId: "1099313837165",
-  appId: "1:1099313837165:web:0f2ceec8e0d52496f33b64",
-  measurementId: "G-P5NRSNHGEZ"
-};
 
-export default firebaseConfig;
