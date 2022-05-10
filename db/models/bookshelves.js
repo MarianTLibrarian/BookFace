7de197e22966b3d21ed389e2fee01ec195f82450
Huijun Lu <lorwegy@gmail.com>
2022-05-10 15:58:32 -0400

    bookshelves endpoints are working

--- db/models/bookshelves.js
+++ db/models/bookshelves.js
@@ -1,8 +1,31 @@
 const db = require('../../db');
+const { collection, getDocs, query, where, addDoc } = require('firebase/firestore');
 module.exports = {
 
-  allBookshelves() {},
-  addBookshelf () {}
+  allBookshelves(req, callback) {
+    const { userId } = req.query;
 
+    const q = query(collection(db, "bookshelves"), where("userId", "==", `${userId}`));
+    getDocs(q)
+      .then(snapshot => {
+        const allBookshelves = [];
+        snapshot.forEach((document) => {
+          allBookshelves.push(document.data().bookshelf)
+        })
+        const result = {
+          userId,
+          results: allBookshelves
+        }
+        callback(null, result);
+      })
+      .catch(err => console.log('Error in get allBookshelves: ', err))
+  },
 
+  addBookshelf(req, callback) {
+    addDoc(collection(db, "bookshelves"), req)
+      .then(snapshot =>
+        callback(null, snapshot)
+      )
+      .catch(err => console.log('Error in addBookshelf: ', err))
+  }
 }
--- server/controllers/bookshelves.js
+++ server/controllers/bookshelves.js
@@ -1,7 +1,25 @@
 const models = require('../../db/models');
 
-module.exports =  {
-  allBookshelves() {},
-  addBookshelf () {}
+module.exports = {
+  allBookshelves(req, res) {
+    models.bookshelves.allBookshelves(req, (err, data) => {
+      if (err) {
+        console.log('controller error from allBookshelves: ', err);
+        res.sendStatus(400)
+      } else {
+        res.status(200).send(data);
+      }
+    })
+  },
+  addBookshelf(req, res) {
+    models.bookshelves.addBookshelf(req.body, (err) => {
+      if (err) {
+        console.log('controller error from allBookshelves: ', err);
+        res.sendStatus(400)
+      } else {
+        res.status(201).send('added');
+      }
+    })
+  }
 
 }
