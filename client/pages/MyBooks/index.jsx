2e6cef26b749bc966f707fd6a2fb01f84a48c52a
Katy Feng <97313452+katyfsy@users.noreply.github.com>
2022-05-11 06:14:08 -0400

    Mybooksapi (#32)
    * fix navbar typo
    
    * myBooks skeleton and api fetches
    
    * update MyBooks
    
    * style sidebar headings

--- client/pages/MyBooks/index.jsx
+++ client/pages/MyBooks/index.jsx
@@ -1,72 +1,83 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import SideBar from './sidebar';
+// import SideBar from './sidebar';
 import Carousel from './carousel';
 import ReadingGoals from './readingGoals';
 import useStore from '../../userStore';
-
+import '../styles/MyBooks.css';
+import '../styles/BookClubDetails.css';
 
 export default function MyBooks() {
 
-  const [books, setBooks] = useState([]);
-  const [currentView, setCurrentView] = useState('');
   const { user, setUser, setToken } = useStore();
+  const [allBooks, setAllBooks] = useState([]);
+  const [bookclubs, setBookclubs] = useState([]);
+  const [bookshelves, setBookshelves] = useState([]);
+  const [currentView, setCurrentView] = useState('All');
 
   const handleClick = (event) => {
     setCurrentView(event.target.innerText);
+  }
 
+  //NOTE: get bookshelves by userId is working
+  const getBookshelves = (uid) => {
+    axios.get('http://localhost:3030/bookshelves', { params: {userId: 1}})
+      .then(({data}) => {
+        const temp = [];
+        for(let i = 0; i < data.results.length; i += 1) {
+          temp.push(data.results[i].title);
+        }
+        setBookshelves(temp);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
 
+  //NOTE: get bookclubs by userId is working
+  const getBookclubs = (uid) => {
+    axios.get('http://localhost:3030/myBookclubs', { params: {userId: "qwew"}})
+    .then(({data}) => {
+      // console.log('bookclubs', data);
+      const temp = [];
+      for(let i = 0; i < data.results.length; i += 1) {
+        temp.push(data.results[i].bookclubInfo.bookclubName);
+      }
+      setBookclubs(temp);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  //NOTE: get books by userId is working
+  //Default data for the book gallery
+  const getBooks = (uid) => {
+  axios.get('http://localhost:3030/books', { params: {userId: 1}})
+    .then(({data}) => {
+      // console.log('books', data);
+      // const container = [];
+      // for(let i = 0; i < data.results.length; i += 1) {
+      //   const temp = {};
+      //   temp.bookshelf = data.results[i].bookshelf;
+      //   temp.img = data.results[i].imageLinks.smallThumbnail;
+      //   container.push(temp);
+      // }
+      setAllBooks(data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
 
-  // const uniqueBookshelves = [];
-  // function removeDuplicates(arr) {
-  //   arr.forEach(element => {
-  //       if (!uniqueBookshelves.includes(element)) {
-  //           uniqueBookshelves.push(element);
-  //       }
-  //   });
-  //   return uniqueBookshelves;
-  // }
-  // removeDuplicates(bookshelves);
-
-  // const getBooks = function() {
-  //   axios.get('/books')
-  //   .then(data => {
-  //     console.log(data)
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  // }
-
-  // const getBookshelves = (uid) => {
-  //   axios.get('/bookshelves', { params: {userId: 1}})
-  //     .then(({data}) => {
-  //       console.log('THIS', data);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     })
-  // }
-
-  const getBookshelves = (userId) => {
-    axios({
-      method: 'get',
-      url: 'https://localhost:3030/bookshelves',
-      params: { userId: '1' }
-    })
-    .then(({ data }) => {
-      console.log('HERE!!!', data)
-    })
-    .catch((err) => {
-      console.log('error on client side')
-    });
-  }
 
   useEffect(() => {
     getBookshelves();
-  })
+    getBooks();
+    getBookclubs();
+  },[])
 
   const style = {
     background: 'url(../assets/header-bg.jpg) no-repeat center center fixed',
@@ -103,37 +114,57 @@ export default function MyBooks() {
 
           <div className='content-container'>
             <div className='content-left'>
-              {/* <div className='my-bookshelves'>
+              <div className='my-bookshelves'>
                 <h2>My Bookshelves</h2>
-                  {uniqueBookshelves.map(shelf => (
+                  <p value={'All'} onClick={handleClick}>All</p>
+                  {bookshelves.map(shelf => (
                     <p
+                      key={Math.random()}
                       value={shelf}
                       onClick={handleClick}
                     >
                       {shelf}
                     </p>
                   ))}
-              </div> */}
+              </div>
 
               <div className='my-book-clubs'>
-                {/* To Do: dynamically render book clubs */}
+
                 <h2>My Book Clubs</h2>
-                  <p>All</p>
-                  <p>Read</p>
-                  <p>Currently Reading</p>
-                  <p>Want To Read</p>
-                  {/* <SideBar /> */}
+                  {bookclubs.map(club => (
+                    <div key={club}>
+                      <Link to='/bookclubdetail' style={{'text-decoration': 'none', 'color': 'black'}}>
+                        {club}
+                      </Link>
+                    </div>
+                  ))}
               </div>
+
               <div className='reading-goal'>
-                <h2>Reading Goals</h2>
+                <Link to='/stats' style={{'text-decoration': 'none', 'color': 'black'}}>
+                  <h2 style={{
+                    'display': 'flex',
+                    'flex-direction': 'row',
+                    'align-items': 'center',
+                    'justify-content': 'space-between'
+                  }}>
+                    Reading Goals
+                    <span
+                      style={{'font-size': '12px', 'padding-right': '1em'}}
+                    >&#9658;</span>
+                  </h2>
+                </Link>
                   <ReadingGoals />
               </div>
+
             </div>
 
             {/* NOTE: Bookshelves get rendered here */}
             <div className='content-right'>
-              <Carousel books={books}/>
-              HEREEE
+              <Carousel
+                selectedBookshelf={currentView}
+                allBooks={allBooks}
+              />
             </div>
 
           </div>
@@ -141,6 +172,5 @@ export default function MyBooks() {
       </div>
     </div>
   )
-
 }
 
