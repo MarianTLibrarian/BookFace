1ad167caa498e6f1eeee469fb20a2dee8f556426
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-11 18:37:42 -0400

    Feature: NavBar search (#42)
    * Add searchbar to navbar
    
    It's ugly rn. Don't worry, we'll fix it tomorrow.
    
    * Hide search on homepage
    
    * Add ability to submit search and see search page

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import AccountMenu from './AccountMenu';
 
 import { signInWithGoogle } from '../Firebase';
 import useStore from '../../client/userStore';
+import SearchBar from '../SearchBar';
 
 export default function NavBar() {
   const { user, setUser, setToken } = useStore();
+  const { pathname } = useLocation();
 
   const handleSignIn = () => {
     if (!user) {
-      return signInWithGoogle()
+      signInWithGoogle()
         .then((result) => {
           if (!result || !result.user || !result.token) throw result;
 
@@ -18,12 +20,12 @@ export default function NavBar() {
 
           localStorage.setItem('user_data', JSON.stringify(newUser));
 
-        setUser(newUser);
-        setToken(token);
-      })
-      .catch(console.error);
+          setUser(newUser);
+          setToken(token);
+        })
+        .catch(console.error);
+    }
   };
-}
 
   return (
     <div className="nav-container">
@@ -37,22 +39,24 @@ export default function NavBar() {
           <div className="clear" />
         </div>
         <div className="menu">
+          <div
+            className="nav-search"
+            style={pathname === '/' ? { visibility: 'hidden' } : { visibility: 'visible' }}
+          >
+            <SearchBar />
+          </div>
           <NavLink exact to="/">
             Home
           </NavLink>
-          {user ?
+          {user ? (
             <NavLink activeClassName="active" to="/mybooks">
               My Books
             </NavLink>
-            : null
-          }
+          ) : null}
           <NavLink activeClassName="active" to="/bookclubs">
             {user ? 'My clubs' : 'Clubs'}
           </NavLink>
-          {user ?
-            <AccountMenu />
-            : <a onClick={handleSignIn} >Sign In</a>
-          }
+          {user ? <AccountMenu /> : <a onClick={handleSignIn}>Sign In</a>}
         </div>
       </div>
     </div>
