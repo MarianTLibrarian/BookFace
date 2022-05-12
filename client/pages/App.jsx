1a47b4497ee32d4cadb2a15e3ef584eb7f69cdaf
Hailee Lu <92131794+HuijunLu@users.noreply.github.com>
2022-05-11 22:07:01 -0400

    Live Chat basic function is implemented  (#47)
    * update home page display and hook up api with database
    
    * calendar is working
    
    * calendar css
    
    * scheduling function is complete
    
    * scheduling function is working!!!
    
    * chat basic function is good

--- client/pages/App.jsx
+++ client/pages/App.jsx
@@ -2,7 +2,6 @@ import React, { Suspense, useEffect, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import useStore from '../userStore';
-import { io } from "socket.io-client";
 
 
 
@@ -14,7 +13,6 @@ const BookDetail = lazy(() => import('./BookDetail'));
 const Home = lazy(() => import('./Home'));
 const MyBooks = lazy(() => import('./MyBooks'));
 
-const socket = io.connect("http://localhost:3030")
 
 function NotFound() {
   return <>You have landed on a page that does not exist</>;
---
+++ client/pages/BookClubDetail/ChatRoom.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      socket.emit("send_message", messageData);
+      setMessageList(messageList.concat(messageData));
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList(messageList.concat(data));
+    });
+  }, [socket, messageList]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <div className="message-container">
+          {messageList.map((messageContent) =>
+
+            <div
+              className="message"
+              id={username === messageContent.author ? "you" : "other"}
+            >
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+
+          )}
+        </div>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
