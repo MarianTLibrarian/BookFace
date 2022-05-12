3563045d2e28ec1df6b2c90f68e20861a21d397c
haleyjung <haleyjung16@gmail.com>
2022-05-11 20:25:13 -0400

    add conditional rendering to calendar stats

--- components/Stats/CalendarStats.jsx
+++ components/Stats/CalendarStats.jsx
@@ -36,43 +36,75 @@ export default function CalendarStats() {
   const calStartEndDate = calendarStats.day;
 
   useEffect(() => {
-    getCalendarStats(1);
+    getCalendarStats(JSON.parse(user).uid);
   }, []);
 
   return (
     <div className="stats-calendar">
       <div>
-        <h3>Your readings this year</h3>
+        <h3 style={{'color': '#808080'}}>Your readings this year</h3>
       </div>
-      <ResponsiveCalendar
-        data={[calendarStats]}
-        from={calStartEndDate}
-        to={calStartEndDate}
-        emptyColor="#eeeeee"
-        colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
-        margin={{
-          top: 40,
-          right: 40,
-          bottom: 40,
-          left: 40,
-        }}
-        yearSpacing={40}
-        monthBorderColor="#ffffff"
-        dayBorderWidth={2}
-        dayBorderColor="#ffffff"
-        legends={[
-          {
-            anchor: 'bottom-right',
-            direction: 'row',
-            translateY: 36,
-            itemCount: 4,
-            itemWidth: 42,
-            itemHeight: 36,
-            itemsSpacing: 14,
-            itemDirection: 'right-to-left',
-          },
-        ]}
-      />
+      {calendarStats.length === 0 ?
+        <ResponsiveCalendar
+          data={[calendarStats]}
+          from='2022-01-09'
+          to='2022-08-09'
+          emptyColor="#eeeeee"
+          colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
+          margin={{
+            top: 40,
+            right: 40,
+            bottom: 40,
+            left: 40,
+          }}
+          yearSpacing={40}
+          monthBorderColor="#ffffff"
+          dayBorderWidth={2}
+          dayBorderColor="#ffffff"
+          legends={[
+            {
+              anchor: 'bottom-right',
+              direction: 'row',
+              translateY: 36,
+              itemCount: 4,
+              itemWidth: 42,
+              itemHeight: 36,
+              itemsSpacing: 14,
+              itemDirection: 'right-to-left',
+            },
+          ]}
+        />
+        :
+        <ResponsiveCalendar
+          data={[calendarStats]}
+          from={calStartEndDate}
+          to={calStartEndDate}
+          emptyColor="#eeeeee"
+          colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
+          margin={{
+            top: 40,
+            right: 40,
+            bottom: 40,
+            left: 40,
+          }}
+          yearSpacing={40}
+          monthBorderColor="#ffffff"
+          dayBorderWidth={2}
+          dayBorderColor="#ffffff"
+          legends={[
+            {
+              anchor: 'bottom-right',
+              direction: 'row',
+              translateY: 36,
+              itemCount: 4,
+              itemWidth: 42,
+              itemHeight: 36,
+              itemsSpacing: 14,
+              itemDirection: 'right-to-left',
+            },
+          ]}
+        />
+      }
     </div>
   );
 }
