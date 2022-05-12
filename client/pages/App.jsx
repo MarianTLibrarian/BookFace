856453ed2c8cdfe85f2061d93dbfe0d19e678ba2
Junpeng Chang <rocchang.jp@gmail.com>
2022-05-11 20:25:32 -0400

    live chat in progress (#44)

--- client/pages/App.jsx
+++ client/pages/App.jsx
@@ -2,6 +2,9 @@ import React, { Suspense, useEffect, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import useStore from '../userStore';
+import { io } from "socket.io-client";
+
+
 
 const NavBar = lazy(() => import('../../components/NavBar'));
 const Stats = lazy(() => import('../../components/Stats'));
@@ -10,6 +13,8 @@ const BookClubs = lazy(() => import('./BookClubs'));
 const BookDetail = lazy(() => import('./BookDetail'));
 const Home = lazy(() => import('./Home'));
 const MyBooks = lazy(() => import('./MyBooks'));
+
+const socket = io.connect("http://localhost:3030")
 
 function NotFound() {
   return <>You have landed on a page that does not exist</>;
@@ -23,7 +28,9 @@ export default function App() {
     if (userData) {
       setUser(userData);
     }
+
   }, []);
+
 
   return (
     <Suspense fallback={<>Loading ...</>}>
