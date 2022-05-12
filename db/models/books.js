2a4312673a18c0ac3ee9b63542e30da7cc73ac2d
Katy Feng <97313452+katyfsy@users.noreply.github.com>
2022-05-12 18:47:18 -0400

    uncomment console logs

--- db/models/books.js
+++ db/models/books.js
@@ -54,7 +54,6 @@ module.exports = {
 
   addBook(req, callback) {
     let {isbn, userId, title, authors, publisher, publishedDate, description, pageCount, categories, imageLinks, language} = req.body;
-    console.log(isbn, userId, title, authors, publisher, publishedDate, description, pageCount, categories, imageLinks, language);
     isbn = parseInt(isbn, 10);
     pageCount = parseInt(pageCount, 10);
     addDoc(collection(db, 'PersonalBookLibrary'), {
@@ -101,7 +100,6 @@ module.exports = {
 
   updateBook(req, callback) {
     let {rating, isbn, userId, bookshelf, startReadDate, endReadDate, readingStatus} = req.body;
-    // console.log(rating, isbn, userId, bookshelf, startReadDate, endReadDate, readingStatus);
     isbn = parseInt(isbn, 10);
     rating = parseInt(rating, 10);
     const q = query(collection(db, 'PersonalBookLibrary'), where("userId", "==", `${userId}`), where("isbn", "==", isbn));
