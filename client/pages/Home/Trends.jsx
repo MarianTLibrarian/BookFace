eb3557f4c5f242a80103c37e5a3e3ab889c7fe6e
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-12 12:16:35 -0400

    Show images from database books or API books

--- client/pages/Home/Trends.jsx
+++ client/pages/Home/Trends.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import '../../../assets/testBook.jpg';
 
 export default function Trends({ book }) {
+  // let bookImage = '../../../assets/testBook.jpg';
+  const [bookImage, setBookImage] = React.useState('../../../assets/testBook.jpg');
+
+  React.useEffect(() => {
+    console.log({ imgs: book.imageLinks });
+
+    // Why does the database have a different structure? (T_T)
+    if (Object.hasOwnProperty.call(book, 'book_image')) {
+      setBookImage(book.book_image);
+    } else if (Object.hasOwnProperty.call(book, 'imageLinks')) {
+      setBookImage(book.imageLinks.thumbnail || book.imageLinks.smallThumbnail);
+    } else {
+      setBookImage('../../../assets/testBook.jpg');
+    }
+  }, [book]);
+
   return (
     <div className="trends-book">
-      <img src={book.book_image} alt="bookcover" />
+      <img src={bookImage} alt="bookcover" />
     </div>
   );
 }
