6ad85a052ba4c238889142a20342d3f2a42beb27
Addison Hernandez <addison.hernandez@gmail.com>
2022-05-13 11:29:23 -0400

    Remove nivo exclusions

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 // https://vitejs.dev/config/
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {},
-    'global': {}
+    global: {},
   },
-  optimizeDeps: {
-    exclude: ['@nivo/colors']
-  },
-})
+});
